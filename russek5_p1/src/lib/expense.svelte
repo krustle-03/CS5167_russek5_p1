afe6d314ab4e836bf351fb0ac83a3c240ee87ab5
krustle-03 <xeno112203@gmail.com>
2025-09-22 14:23:04 -0400

    Removed the invoice entry section for final commit

--- russek5_p1/src/lib/expense.svelte
+++ russek5_p1/src/lib/expense.svelte
@@ -4,29 +4,15 @@
     import SingleCostEntry from './SingleCost.svelte';
     import { currentPage } from './stores.js';
 
-    let mode = -1 // Default to no mode selected
+    let mode = 1 // Default to no mode selected
 </script>
 
 
 <br />
 
-<div class="centered-items">
-  <div class="mode-select">
-    <label for="mode-select-dropdown">Select Mode:</label>
-    <select id="mode-select-dropdown" bind:value={mode}>
-      <option value="1">Receipt Mode</option>
-      <option value="2">Single Cost Entry</option>
-      <!-- Add more modes here as needed -->
-    </select>
-  </div>
-</div>
 
 {#if mode == 1}
   <Receipt />
-{:else if mode == 2}
-  <!-- <SingleCostEntry /> -->
-  <p>Single Cost Entry mode is under construction.</p>
-  <SingleCost />
 
 {:else}
   <h1>Please select a mode using the dropdown above to begin.</h1>
